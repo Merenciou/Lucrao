53afda5e2f7efdd63b7d645114f3da1a85751a1b
Merenciou <vitor.merencio.2012@hotmail.com>
2024-10-22 22:18:43 -0300

    layout changes on navbar

--- lib/components/navbar/custom_navbar.dart
+++ lib/components/navbar/custom_navbar.dart
@@ -13,77 +13,88 @@ class _CustomNavbarState extends State<CustomNavbar> {
   Widget build(BuildContext context) {
     var deviceData = MediaQuery.of(context);
     var screenSize = deviceData.size;
-    return Container(
-      width: double.infinity,
-      height: screenSize.height / 11,
-      color: const Color(0xFFEEDF7A),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: IconButton(
-              onPressed: () {
-                CustomNavbarController.instance.routeSettings();
-                Navigator.of(context).pushNamed('/settingspage');
-              },
-              style: IconButton.styleFrom(
-                backgroundColor:
-                    CustomNavbarController.instance.navItemIndex == 0
-                        ? const Color(0xFFDEE5D4)
-                        : const Color(0xFFEEDF7A),
-              ),
-              icon: Icon(
-                Icons.settings_rounded,
-                size:
-                    CustomNavbarController.instance.navItemIndex == 0 ? 60 : 50,
-                color: const Color(0xFF000000),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10, left: 16, right: 16),
+      child: Container(
+        width: double.infinity,
+        height: screenSize.height / 11,
+        decoration: const BoxDecoration(
+          color: Color(0xFFEEDF7A),
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                onPressed: () {
+                  CustomNavbarController.instance.routeSettings();
+                  Navigator.of(context).pushNamed('/settingspage');
+                },
+                style: IconButton.styleFrom(
+                  backgroundColor:
+                      CustomNavbarController.instance.navItemIndex == 0
+                          ? const Color(0xFFDEE5D4)
+                          : const Color(0xFFEEDF7A),
+                ),
+                icon: Icon(
+                  Icons.settings_rounded,
+                  size: CustomNavbarController.instance.navItemIndex == 0
+                      ? 60
+                      : 50,
+                  color: const Color(0xFF000000),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: IconButton(
-              onPressed: () {
-                CustomNavbarController.instance.routeHome();
-                Navigator.of(context).pushNamed('/homepage');
-              },
-              style: IconButton.styleFrom(
-                backgroundColor:
-                    CustomNavbarController.instance.navItemIndex == 1
-                        ? const Color(0xFFDEE5D4)
-                        : const Color(0xFFEEDF7A),
-              ),
-              icon: Icon(
-                Icons.home_rounded,
-                size:
-                    CustomNavbarController.instance.navItemIndex == 1 ? 60 : 50,
-                color: const Color(0xFF000000),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: IconButton(
-              onPressed: () {
-                CustomNavbarController.instance.routeAlert();
-                Navigator.of(context).pushNamed('/alertspage');
-              },
-              style: IconButton.styleFrom(
-                backgroundColor:
-                    CustomNavbarController.instance.navItemIndex == 2
-                        ? const Color(0xFFDEE5D4)
-                        : const Color(0xFFEEDF7A),
-              ),
-              icon: Icon(
-                Icons.notifications_rounded,
-                size:
-                    CustomNavbarController.instance.navItemIndex == 2 ? 60 : 50,
-                color: const Color(0xFF000000),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                onPressed: () {
+                  CustomNavbarController.instance.routeHome();
+                  Navigator.of(context).pushNamed('/homepage');
+                },
+                style: IconButton.styleFrom(
+                  backgroundColor:
+                      CustomNavbarController.instance.navItemIndex == 1
+                          ? const Color(0xFFDEE5D4)
+                          : const Color(0xFFEEDF7A),
+                ),
+                icon: Icon(
+                  Icons.home_rounded,
+                  size: CustomNavbarController.instance.navItemIndex == 1
+                      ? 60
+                      : 50,
+                  color: const Color(0xFF000000),
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                onPressed: () {
+                  CustomNavbarController.instance.routeAlert();
+                  Navigator.of(context).pushNamed('/alertspage');
+                },
+                style: IconButton.styleFrom(
+                  backgroundColor:
+                      CustomNavbarController.instance.navItemIndex == 2
+                          ? const Color(0xFFDEE5D4)
+                          : const Color(0xFFEEDF7A),
+                ),
+                icon: Icon(
+                  Icons.notifications_rounded,
+                  size: CustomNavbarController.instance.navItemIndex == 2
+                      ? 60
+                      : 50,
+                  color: const Color(0xFF000000),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
