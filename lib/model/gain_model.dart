3d1003e71d3dfdc11b03c2e37144187d3c568450
Merenciou <vitor.merencio.2012@hotmail.com>
2024-11-11 17:03:08 -0300

    api created to consume user datas

---
+++ lib/model/gain_model.dart
@@ -0,0 +1,64 @@
+class GainModel {
+  final List<double>? januaryGain;
+  final List<double>? februaryGain;
+  final List<double>? marchGain;
+  final List<double>? aprilGain;
+  final List<double>? mayGain;
+  final List<double>? juneGain;
+  final List<double>? julyGain;
+  final List<double>? augustGain;
+  final List<double>? septemberGain;
+  final List<double>? octoberGain;
+  final List<double>? novemberGain;
+  final List<double>? decemberGain;
+
+  GainModel({
+    this.januaryGain,
+    this.februaryGain,
+    this.marchGain,
+    this.aprilGain,
+    this.mayGain,
+    this.juneGain,
+    this.julyGain,
+    this.augustGain,
+    this.septemberGain,
+    this.octoberGain,
+    this.novemberGain,
+    this.decemberGain,
+  });
+
+  // List<double> januaryGain = [
+  //   0,
+  //   10,
+  //   20,
+  //   30,
+  //   40,
+  //   50,
+  //   60,
+  //   70,
+  //   80,
+  //   90,
+  //   100,
+  //   110,
+  //   120,
+  //   130,
+  //   140,
+  //   150,
+  //   160,
+  //   170,
+  //   180,
+  //   190,
+  //   200,
+  //   210,
+  //   220,
+  //   230,
+  //   240,
+  //   250,
+  //   260,
+  //   270,
+  //   280,
+  //   290,
+  //   300,
+  //   310,
+  // ];
+}
