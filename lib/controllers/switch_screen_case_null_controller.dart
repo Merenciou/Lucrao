24a63386329ead3c99e077238d0ae4b633b97204
Merenciou <vitor.merencio.2012@hotmail.com>
2024-11-28 15:31:58 -0300

    added switch screen in case car datas is null

---
+++ lib/controllers/switch_screen_case_null_controller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:lucrao/controllers/autonomy_controller.dart';
+import 'package:lucrao/screens/location_screen/location_null_screen.dart';
+import 'package:lucrao/screens/location_screen/location_screen.dart';
+
+class SwitchScreenCaseNull extends StatefulWidget {
+  const SwitchScreenCaseNull({super.key});
+
+  @override
+  State<SwitchScreenCaseNull> createState() => _SwitchScreenCaseNullState();
+}
+
+class _SwitchScreenCaseNullState extends State<SwitchScreenCaseNull> {
+  late Future<double?> carData;
+  @override
+  void initState() {
+    carData = AutonomyController().getAutonomy();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: carData,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return const LocationScreen();
+        } else if (snapshot.hasError) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          return const LocationNullScreen();
+        }
+      },
+    );
+  }
+}
